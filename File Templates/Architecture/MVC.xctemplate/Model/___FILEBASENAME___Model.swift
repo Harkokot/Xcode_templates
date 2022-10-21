111dd969fdd2d388917301931e497a2103b70854
ndumkin <79084750+ndumkin@users.noreply.github.com>
2022-10-21 15:35:28 +0300

    first commit

---
+++ File Templates/Architecture/MVC.xctemplate/Model/___FILEBASENAME___Model.swift
@@ -0,0 +1,12 @@
+//  ___FILEHEADER___
+
+import Foundation
+
+public struct ___FILEBASENAMEASIDENTIFIER___ {
+
+}
+
+//MARK: - ___FILEBASENAMEASIDENTIFIER___ extension
+extension ___FILEBASENAMEASIDENTIFIER___{
+
+}
---
+++ File Templates/Architecture/MVC.xctemplate/View/___FILEBASENAME___View.swift
@@ -0,0 +1,11 @@
+//  ___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIView {
+
+    //MARK: View configuration
+    private func setupView(){
+
+    }
+}
